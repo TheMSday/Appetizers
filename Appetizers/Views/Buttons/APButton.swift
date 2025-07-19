a55c46e827b17831bbef1609cecfbeb828773cd6
themsday <themsday@gmail.com>
2025-07-19 12:25:23 +0200

    next

---
+++ Appetizers/Views/Buttons/APButton.swift
@@ -0,0 +1,18 @@
+//
+//  APButton.swift
+//  Appetizers
+//
+//  Created by Mehmet Güngörmüş on 19.07.25.
+//
+
+import SwiftUI
+
+struct APButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    APButton()
+}
